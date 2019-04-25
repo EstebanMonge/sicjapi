2ae0d53411116b0fc9cba3bd002ab1f4efafa473
Esteban Monge <estebanmonge@riseup.net>
2019-04-24 23:35:52 -0600

    Change welcome controller to get a form with search

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+
+	public function data_submitted() {
+		$id=$this->input->post('dname');
+		if ($this->input->post('type') == "fisica") {
+			redirect("consulta/fisica/".$id);
+		}
+        	if ($this->input->post('type') == "juridica") {
+                	redirect("consulta/juridica/".$id);
+        	}
 	}
 }
